78189ff026ef71dd320c509f9e48e135709827e6
saiful Islam <saifulmeraz897@gmail.com>
2022-01-12 04:57:47 +0600

    complete project

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -1,13 +1,25 @@
 <template>
-  <h1 v-for="(country,index) in countries" :key="index">
-      {{ country.Country }}
-  </h1>
+    <select @change="onChange" v-model="selected" class="form-select mt-10 block w-full border p-3 rounded">
+        <option value="0">Select Country</option>
+        <option v-for="(country,index) in countries" :key="index" :value="country.ID">{{ country.Country }}</option>
+    </select>
 </template>
 
 <script>
 export default {
     name:"CountrySelect",
-    props:['countries']
+    props:['countries'],
+    data(){
+        return {
+            selected:0
+        }
+    },
+    methods:{
+        onChange(){
+            const country =this.countries.find(item =>item.ID===this.selected)
+            this.$emit('get-country',country)
+        }
+    }
 
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,14 @@
     <main v-if="!loading">
       <DateAndTime :text="title" :dataDate="dataDate"/>
       <DataBoxes :state="state" />
-      <CountrySelect :countries="countries" />
+      <CountrySelect @get-country="getCountryData" :countries="countries" />
+
+      <button
+      @click="clearValue" 
+      v-if="state.Country"
+      class="bg-green-700 text-white rounded p-3 mt-4 mx-auto flex focus:outline-none hover:bg-green-600">
+        Clear History
+      </button>
     </main>
     <main v-else class="flex flex-col align-center justify-center text-center">
       <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -47,6 +54,17 @@ export default {
       const res = await fetch('https://api.covid19api.com/summary')
       const data = await res.json()
       return data
+    },
+    getCountryData(country){
+      this.state=country
+      this.title=country.Country
+    },
+    async clearValue(){
+      this.loading=true
+      const data= await this.fetchingDataFromApi()
+      this.title='Global'
+      this.state=data.Global
+      this.loading=false
     }
   }
   
